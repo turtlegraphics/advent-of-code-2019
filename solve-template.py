bfabd5c0f2b79ad392c72200db02ea05952aa6c9
Bryan Clair <bryan@slu.edu>
2019-11-28 01:17:02 -0600

    New template with argparse.

--- solve-template.py
+++ solve-template.py
@@ -4,14 +4,38 @@
 #
 # Day --
 #
-import sys
+import argparse
 
 if __name__ == "__main__":
-    filename = 'input.txt'
-    if len(sys.argv) > 1:
-        filename = sys.argv[1]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--verbose",
+                        action = "count",
+                        dest = "verbose",
+                        default = 1,
+                        help = "Set verbosity level (-v, -vv, -vvv,...)")
+    parser.add_argument("-q", "--quiet",
+                        action = "store_const",
+                        const = 0,
+                        dest = "verbose",
+                        help = "Suppress output.")
+    
+    parser.add_argument("-p", "--part",
+                        action="store",
+                        dest = "part",
+                        default = 1,
+                        type = int,
+                        help = "Which part of the problem to solve (1 or 2)")
+    
+    parser.add_argument("file",
+                        nargs = "?",
+                        default = "input.txt",
+                        help = "Problem input file (optional).")
+                        
+    args = parser.parse_args()
 
-    data = open(filename).readlines()
+    print args
+
+    data = open(args.file).readlines()
 
     for line in data:
         print line
