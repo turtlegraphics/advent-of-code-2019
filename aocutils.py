cacea2bd8ecfd3bfd0aea8edd77e9aa56d6622d8
Bryan Clair <bryan@slu.edu>
2019-12-03 21:06:41 -0600

    Moved argument parser to utils file, smaller template

---
+++ aocutils.py
@@ -0,0 +1,37 @@
+#
+# Advent of Code 2019
+# Bryan Clair
+#
+# Utilities
+#
+from argparse import ArgumentParser
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument("-v", "--verbose",
+                        action = "count",
+                        dest = "verbose",
+                        default = 1,
+                        help = "Set verbosity level (-v, -vv, -vvv,...)")
+    parser.add_argument("-q", "--quiet",
+                        action = "store_const",
+                        const = 0,
+                        dest = "verbose",
+                        help = "Suppress output.")
+    
+    parser.add_argument("-p", "--part",
+                        action="store",
+                        dest = "part",
+                        default = 1,
+                        type = int,
+                        help = "Which part of the problem to solve (1 or 2)")
+    
+    parser.add_argument("file",
+                        nargs = "?",
+                        default = "input.txt",
+                        help = "Problem input file (optional).")
+    args = parser.parse_args()
+    if args.verbose > 2:
+        print args
+
+    return args
--- solve-template.py
+++ solve-template.py
@@ -4,38 +4,13 @@
 #
 # Day --
 #
-import argparse
+import sys
+sys.path.append("..")
+import aocutils
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-v", "--verbose",
-                        action = "count",
-                        dest = "verbose",
-                        default = 1,
-                        help = "Set verbosity level (-v, -vv, -vvv,...)")
-    parser.add_argument("-q", "--quiet",
-                        action = "store_const",
-                        const = 0,
-                        dest = "verbose",
-                        help = "Suppress output.")
-    
-    parser.add_argument("-p", "--part",
-                        action="store",
-                        dest = "part",
-                        default = 1,
-                        type = int,
-                        help = "Which part of the problem to solve (1 or 2)")
-    
-    parser.add_argument("file",
-                        nargs = "?",
-                        default = "input.txt",
-                        help = "Problem input file (optional).")
-                        
-    args = parser.parse_args()
+args = aocutils.parse_args()
 
-    print args
+inputlines = [x.strip() for x in open(args.file).readlines()]
 
-    data = open(args.file).readlines()
-
-    for line in data:
-        print line
+for line in inputlines:
+    print line
