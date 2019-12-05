1824f0e9c06ce579aa80b11d178a8f00ceab289b
Bryan Clair <bryan@slu.edu>
2019-12-05 01:02:55 -0600

    Minor update to use new init-from-file functionality

--- day5/solve.py
+++ day5/solve.py
@@ -11,19 +11,13 @@ import intcode
 
 args = aocutils.parse_args()
 
-inputlines = [x.strip() for x in open(args.file).readlines()]
-
-mem = []
-for line in inputlines:
-    mem.extend([int(x) for x in line.split(',')])
-
 # part 1
 print "Part 1"
-machine = intcode.Machine(mem,input=[1])
+machine = intcode.Machine(args.file,input=[1])
 machine.run()
 print machine.output
 
 print "Part 2"
-machine = intcode.Machine(mem,input=[5],debug=True)
+machine = intcode.Machine(args.file,input=[5])
 machine.run()
 print machine.output
