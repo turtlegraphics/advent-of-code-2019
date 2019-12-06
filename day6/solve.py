47db842fdca1920900ab98070d44857dbddd3e92
Bryan Clair <bryan@slu.edu>
2019-12-05 23:24:16 -0600

    Recursion for the win (of 2nd place (in our group))

--- day6/solve.py
+++ day6/solve.py
@@ -12,5 +12,56 @@ args = aocutils.parse_args()
 
 inputlines = [x.strip() for x in open(args.file).readlines()]
 
+orbiting = {}
+reverse = {}
+
 for line in inputlines:
-    print line
+    center, orbiter = line.split(')')
+    if center in orbiting:
+        orbiting[center].append(orbiter)
+    else:
+        orbiting[center] = [orbiter]
+    reverse[orbiter] = center
+
+if args.verbose > 1:
+    print orbiting
+
+def orbits(center,level):
+    tot = level
+    if center not in orbiting:
+        return tot
+    for p in orbiting[center]:
+        tot += orbits(p,level+1)
+    return tot
+
+print "Part 1:",orbits('COM',0)
+
+path1 = []
+p = "YOU"
+while p != "COM":
+    path1.append(p)
+    p = reverse[p]
+if args.verbose > 1:
+    print path1
+
+path2 = []
+p = "SAN"
+while p != "COM":
+    path2.append(p)
+    p = reverse[p]
+if args.verbose > 1:
+    print path2
+
+intersect = [p for p in path1 if p in path2]
+if args.verbose > 1:
+    print intersect
+common = intersect[0]
+if args.verbose > 1:
+    print path1.index(common)
+    print path2.index(common)
+
+print "Part 2:",path1.index(common)+path2.index(common)-2
+
+
+
+
