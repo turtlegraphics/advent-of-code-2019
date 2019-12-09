7466bc7ae5d5997b72441ea37cfab9450a64203e
Bryan Clair <bryan@slu.edu>
2019-12-09 03:55:11 -0600

    Done, but general memory management unimplemented.

--- day9/solve.py
+++ day9/solve.py
@@ -8,11 +8,18 @@ import sys
 sys.path.append("..")
 import aocutils
 import intcode
+from itertools import permutations
 
 args = aocutils.parse_args()
 
-inputlines = [x.strip() for x in open(args.file).readlines()]
+with open(args.file, 'r') as memfile:
+    content = memfile.read()
+    mem = [int(x) for x in content.split(',')]
 
-for line in inputlines:
-    print line
+BOOST = intcode.Machine(mem + [0]*1000,input=[1])
+BOOST.runq()
+print BOOST.output
 
+BOOST = intcode.Machine(mem + [0]*1000,input=[2])
+BOOST.runq()
+print BOOST.output
