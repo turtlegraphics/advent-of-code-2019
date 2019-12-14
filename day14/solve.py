f253972cc505019d83e0d3187bc6971a356b7d55
Bryan Clair <bryan@slu.edu>
2019-12-14 00:19:03 -0600

    By-hand binary search no longer required.

--- day14/solve.py
+++ day14/solve.py
@@ -14,7 +14,6 @@ stuff = ['ORE']
 inputlines = [x.strip() for x in open(args.file).readlines()]
 for line in inputlines:
     stuff.append(line.split("=>")[1].split()[1])
-print stuff
 
 inputlines = [x.strip() for x in open(args.file).readlines()]
 
@@ -33,38 +32,62 @@ for line in inputlines:
 
     reactions[product] = reaction
 
-print reactions
+def how_much_ore(fuel):
+    """
+    returns amount of ORE needed to make given amount of fuel
+    """
+    next = stuff.index("FUEL")
+    need = [0]*len(stuff)
+    need[next] = fuel
+    while next:
+        making = stuff[next]
+        quantity = need[stuff.index(making)]
+        if args.verbose > 1:
+            print 'Making',quantity,'of',making
 
-need = [0]*len(stuff)
-need[stuff.index("FUEL")] = 8845261
-# print need
+        using = reactions[making]
+        produces = -using[stuff.index(making)]
 
-def firstpos(v):
-    """Find maximum required need"""
-    most = max(v[1:])
-    if most > 0:
-        return v.index(most)
-    return None
+        multiplier = quantity // produces
+        if multiplier * produces < quantity:
+            multiplier += 1
+        assert(multiplier * produces >= quantity)
 
-next = firstpos(need)
-while next:
-    making = stuff[next]
-    quantity = need[stuff.index(making)]
-    print 'Making',quantity,'of',making
-    using = reactions[making]
-    produces = -using[stuff.index(making)]
+        for i in range(len(stuff)):
+            need[i] += multiplier * using[i]
 
-    multiplier = quantity // produces
-    if multiplier * produces < quantity:
-        multiplier += 1
-    assert(multiplier * produces >= quantity)
+        if args.verbose > 2:
+            print need
 
-    print '    multiplier',multiplier
-    for i in range(len(stuff)):
-        need[i] += multiplier * using[i]
+        most = max(need[1:])
+        if most > 0:
+            next = need.index(most)
+        else:
+            next = None
+    return need[0]
 
-    print need
-    next = firstpos(need)
 
-print 'Used ORE =',need[0]
-print need[0] <= 1000000000000
+# Part 1
+ore = how_much_ore(1)
+print 'Part 1: To make one FUEL, we need',ore,'ORE'
+
+# Part 2
+# Refactored this post-contest.  When doing this live, I did the
+# binary search by hand!
+#
+ore_max = 1000000000000
+high = 1
+low = 1
+while how_much_ore(high) <= ore_max:
+    high *= 2
+
+while high > low + 1:
+    mid = low + (high-low)//2
+    if how_much_ore(mid) <= ore_max:
+        low = mid
+    else:
+        high = mid
+    if args.verbose > 2:
+        print (low,high)
+
+print 'Part 2: With',ore_max,'ORE, we can make',low,'FUEL'
