34bb8ac4171aa774d91f3382edeeecbc50ed95e4
Bryan Clair <bryan@slu.edu>
2019-12-16 01:53:51 -0600

    Massive improvement, but still not enough.

--- day16/solve.py
+++ day16/solve.py
@@ -7,6 +7,7 @@
 import sys
 sys.path.append("..")
 import aocutils
+from fractions import gcd
 
 args = aocutils.parse_args()
 
@@ -16,23 +17,42 @@ for line in inputlines:
     signal = [int(x) for x in list(line)]
 
 print(signal)
-
+siglen = len(signal)
+print('signal length',siglen)
 
 mask_base = [0,1,0,-1]
 
 def mask(stage,index):
     return mask_base[(index + 1)//(stage + 1) % 4]
 
-def fft(signal):
-    v = list(signal)
-    for stage in range(len(signal)):
-        dot = 0
-        for index in range(len(signal)):
-            dot += signal[index]*mask(stage,index)
-        v[stage] = abs(dot) % 10
-    return v
+def lcm(a,b):
+    return a * b // gcd(a,b)
 
-for i in range(100):
-    signal = fft(signal)
+def fft_slow(row,width):
+    sum = 0
+    for i in range(width):
+        sum += signal[i % siglen]*mask(row,i)
+    return sum
 
-print signal
+def fft(row,width):
+    period = lcm(siglen, 4*(row+1))
+    # print 'period',period
+    psum = 0
+    for i in range(period):
+        psum += signal[i % siglen]*mask(row,i)
+
+    # print 'period sum',psum
+    sum = psum * (width // period)
+    for i in range(width % period):
+        sum += signal[i % siglen]*mask(row,i)
+
+    return sum
+
+for row in range(400):
+    print 'row',row,'period',lcm(siglen, 4*(row+1))
+    for width in range(400):
+        assert(fft(row,width) == fft_slow(row,width))
+    print
+
+
+    
