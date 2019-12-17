588c6ba8febe848727bf0483fd27cd773a38cffc
Bryan Clair <bryan@slu.edu>
2019-12-16 21:30:22 -0600

    Solve in R.  Just under 5 hour run time for part 2.

--- day16/solve.R
+++ day16/solve.R
@@ -1,6 +1,9 @@
 library(data.table)
 library(readr)
 
+# How many fft's to apply before the message (Part 2 requires 100)
+rounds <- 100
+
 instring <- read_file("/Users/bryan/Documents/Puzzles/Advent 2019/day16/input.txt")
 input <- as.integer(head(unlist(strsplit(instring,split="")),n=-1))
 print(input)
@@ -11,7 +14,7 @@ print(offset)
 fft <- function(signal) {
   N <- length(signal);
   sums <- c(0,cumsum(signal));
-  result <- 0;
+  result <- 0
   for (d in seq((N-1)%/%2,0,-1)) {
     lead <- seq(2*d+2,N+1,2*(d+1));
     trail <- seq(2*d+1,N,2*d+1);
@@ -19,14 +22,17 @@ fft <- function(signal) {
     pad <- rep(N+1,curlen-length(lead));
     lead <- c(lead,pad);
     result <- c(result,rep(0,curlen-length(result)));
-    result <- result + ((-1)^d*(sums[lead] - sums[trail]))
+    result[1:curlen] <- result[1:curlen] + ((-1)^d*(sums[lead] - sums[trail]))
   }
   abs(result) %% 10;
 }
 
-for (i in 1:100) {
-  signal <- fft(signal);
+totaltime <- system.time(0)
+for (i in 1:rounds) {
+  t <- system.time(signal <- fft(signal));
+  totaltime <- totaltime + t
   print(i);
+  print(totaltime);
 }
 
 print(signal[(offset+1):(offset+8)])
